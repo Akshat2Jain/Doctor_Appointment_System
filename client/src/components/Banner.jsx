8209cd2ecbe5c78aecb1aeb73d3d17e245b5d96c
Paula Moyano <Paula@Rodrigos-Mini.lan>
2023-08-01 10:33:24 -0400

    add services and banner w/basic css

---
+++ client/src/components/Banner.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styles/Banner.css';
+
+const Banner = () => {
+  return (
+    <div className="banner">
+      <h1 className="banner-title">Welcome to the Doctor Appointment System</h1>
+      <p className="banner-subtitle">Book your appointments online and experience top-quality healthcare.</p>
+    </div>
+  );
+};
+
+export default Banner;
--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -2,15 +2,21 @@ import React from "react";
 import "../styles/Homepage.css";
 import Navbar from "../components/Navbar";
 import Testimonials from "../components/Testimonial";
+import Services from "../components/Services";
+import Banner from "../components/Banner";
 
 const HomePage = () => {
   return (
     <>
       <Navbar />
       <div className="home-page">
+        {/* <Banner/> */}
         <h1>Welcome to the Doctor Appointment System</h1>
+        {/* <Services /> */}
+        {/* <Testimonials /> */}
+
       </div>
-      <Testimonials/>
+
     </>
   );
 };
